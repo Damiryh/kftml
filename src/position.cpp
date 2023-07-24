036603ab4be7ef0eaf390de9b683a1efff9558ae
Damir Alimbekov <damir.alimbekov.2002@gmail.com>
2023-07-24 20:58:14 +0500

    Изменил табы на пробелы в отступах

--- src/position.cpp
+++ src/position.cpp
@@ -5,5 +5,19 @@ using namespace kftm;
 Position::Position(std::string path, unsigned int line, unsigned int column, unsigned int pos):
 	path(path), line(line), column(column), pos(pos) {}
 
+Position::Position(std::string path):
+    path(path), line(1), column(1), pos(0) {}
+
 Position::Position():
 	path(""), line(1), column(1), pos(0) {}
+
+void Position::next(int step) {
+    column += step;
+    pos += step;
+}
+
+void Position::newLine() {
+    column = 1;
+    line++;
+    pos++;
+}
