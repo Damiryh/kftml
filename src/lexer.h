d4894fb27a12981a74639923e7452c633599fa84
Damir Alimbekov <damir.alimbekov.2002@gmail.com>
2023-07-24 21:00:29 +0500

    Реализован лексический анализатор

---
+++ src/lexer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include "token.h"
+
+namespace kftm {
+	class Lexer {
+		const std::string &source;
+		std::string::const_iterator ch;
+		std::vector<Token> &tokens;
+		Position currentPos;
+
+		void tokenizeWord();
+        void tokenizeNumber();
+
+	public:
+		Lexer(const std::string &source, std::vector<Token> &tokens);
+		void tokenize();
+	};
+}
